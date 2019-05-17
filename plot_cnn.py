ba4fad72911788c7d1274c7ede162e5251e346f6
Yves Renier <yves.renier@unige.ch>
2019-05-17 16:13:35 +0200

    fix model name

--- plot_cnn.py
+++ plot_cnn.py
@@ -70,8 +70,8 @@ def fit_sum_norm_gaussian(
 
 
 def run_prediction(
-        run_name, photon_rate_mhz=30,sampling_rate_mhz=250, batch_size=400, noise_lsb=1.05,
-        bin_size_ns=0.5, n_sample=90, sigma_smooth_photon_ns=0.
+        run_name, pe_rate_mhz=30,sampling_rate_mhz=250, batch_size=400, noise_lsb=1.05,
+        bin_size_ns=0.5, n_sample=90, sigma_smooth_pe_ns=0.
 ):
     # toy parameters
     n_sample_init = 20
@@ -79,12 +79,12 @@ def run_prediction(
 
     generator = generator_for_training(
         n_event=None, batch_size=batch_size, n_sample=n_sample + n_sample_init,
-        n_sample_init=n_sample_init, photon_rate_mhz=photon_rate_mhz,
+        n_sample_init=n_sample_init, pe_rate_mhz=pe_rate_mhz,
         bin_size_ns=bin_size_ns, sampling_rate_mhz=sampling_rate_mhz,
         amplitude_gain=amplitude_gain, noise_lsb=noise_lsb,
-        sigma_smooth_photon_ns=sigma_smooth_photon_ns
+        sigma_smooth_pe_ns=sigma_smooth_pe_ns
     )
-    waveform, photon = next(generator)
+    waveform, pe = next(generator)
 
     model = tf.keras.models.load_model(
         './Model/' + run_name + '.h5',
@@ -94,126 +94,159 @@ def run_prediction(
         }
     )
     print('model ' + run_name + ' is loaded')
-    predict_photon = model.predict(waveform)
-    loss = model.evaluate(x=waveform, y=photon)
+    predict_pe = model.predict(waveform)
+    loss = model.evaluate(x=waveform, y=pe)
     print('ĺoss=', loss)
-    return waveform, photon, predict_photon,
+    return waveform, pe, predict_pe,
 
 
-def photon_stat(bin_size_ns, photon_truth, predict_photon):
-    if photon_truth.ndim > 1:
-        raise ValueError('photon_truth must be a vector')
-    if predict_photon.ndim > 1:
-        raise ValueError('predict_photon must be a vector')
-    n_bin = len(photon_truth)
-    t_photon_ns = np.arange(0, n_bin) * bin_size_ns
+def pe_stat(bin_size_ns, pe_truth, predict_pe):
+    if pe_truth.ndim > 1:
+        raise ValueError('pe_truth must be a vector')
+    if predict_pe.ndim > 1:
+        raise ValueError('predict_pe must be a vector')
+    n_bin = len(pe_truth)
+    t_pe_ns = np.arange(0, n_bin) * bin_size_ns
     t_predict = []
-    temp = photon_truth.flatten().copy()
+    temp = pe_truth.flatten().copy()
     while np.sum(temp) > 0:
-        t_predict.extend(t_photon_ns[temp >= 1])
+        t_predict.extend(t_pe_ns[temp >= 1])
         temp[temp >= 1] -= 1
     offset, center, width, d_offset, d_center, d_width = fit_sum_norm_gaussian(
-        t_photon_ns[50:-50],
-        predict_photon[50:-50] / bin_size_ns,
-        int(np.sum(photon_truth[50:-50])),
+        t_pe_ns[50:-50],
+        predict_pe[50:-50] / bin_size_ns,
+        int(np.sum(pe_truth[50:-50])),
         cen_init=t_predict
     )
     return offset, center, width, d_offset, d_center, d_width
 
 
 def plot_prediction(
-        bin_size_ns, photon_truth, predict_photon, t_samples_ns, waveform,
+        bin_size_ns, pe_truth, predict_pe, t_samples_ns, waveform,
         title=None, filename=None
 ):
-    offset, center, width, d_offset, d_center, d_width = photon_stat(
-        bin_size_ns, photon_truth[0, :], predict_photon[0, :]
+    offset, center, width, d_offset, d_center, d_width = pe_stat(
+        bin_size_ns, pe_truth[0, :], predict_pe[0, :]
     )
-    n_bin = photon_truth.shape[1]
-    t_photon_ns = np.arange(0, n_bin) * bin_size_ns
+    n_bin = pe_truth.shape[1]
+    t_pe_ns = np.arange(0, n_bin) * bin_size_ns
 
     fig, axes = plt.subplots(3, 1, sharex='all', figsize=(8, 8))
     axes[0].plot(t_samples_ns, waveform[0, :])
     axes[0].set_ylabel('waveform [LSB]')
-    axes[1].plot(t_photon_ns, photon_truth[0, :], label="truth")
+    axes[1].plot(t_pe_ns, pe_truth[0, :], label="truth")
     axes[1].plot(
-        t_photon_ns[50:-50],
-        predict_photon[0, 50:-50],
-        label="proba pred."
+        t_pe_ns[50:-50],
+        predict_pe[0, 50:-50],
+        label="probability predicted"
     )
     axes[1].plot(
-        t_photon_ns[50:-50],
-        sum_norm_gaussian(t_photon_ns[50:-50], offset, *center, *width) * bin_size_ns,
-        label="gaussian fit",
+        t_pe_ns[50:-50],
+        sum_norm_gaussian(t_pe_ns[50:-50], offset, *center, *width) * bin_size_ns,
+        '--', label="gaussian fit",
     )
-    axes[1].set_ylabel('#photons')
+    axes[1].set_ylabel('# pe')
     axes[1].legend()
-    if title is not None:
-        axes[1].set_title(title)
-    axes[2].plot(t_photon_ns[50:-50], np.cumsum(photon_truth[0, 50:-50]), label="truth")
-    axes[2].plot(t_photon_ns[50:-50], np.cumsum(predict_photon[0, 50:-50]), label="prediction")
     axes[2].plot(
-        t_photon_ns[50:-50], np.cumsum(photon_truth[0, 50:-50]) - np.cumsum(predict_photon[0, 50:-50]),
-        label="difference"
+        t_pe_ns[50:-50], np.cumsum(pe_truth[0, 50:-50]),
+        label="truth"
     )
-    axes[2].plot()
+    axes[2].plot(
+        t_pe_ns[50:-50], np.cumsum(predict_pe[0, 50:-50]),
+        label="prediction"
+    )
+    axes[2].plot(
+        t_pe_ns[50:-50],
+        np.cumsum(pe_truth[0, 50:-50]) - np.cumsum(predict_pe[0, 50:-50]),
+        label="truth - prediction"
+    )
     axes[2].set_xlabel('time [ns]')
-    axes[2].set_ylabel('cumulative # photons')
+    axes[2].set_ylabel('cumulative # pe')
     axes[2].legend()
+    if title is not None:
+        axes[0].set_title(title)
     plt.tight_layout()
     if filename is not None:
         plt.savefig(filename)
     else:
         plt.show()
+    plt.close(fig)
 
 
-def plot_probability_check(predict_photon, photon_truth,
-                           title=None, filename=None):
-    plt.figure(figsize=(8, 6))
-    histo_pred, pred_bins = np.histogram(
-        predict_photon, np.linspace(0, np.max(predict_photon),50)
+def plot_probability_check(predict_pe, pe_truth, n_bin=50,
+                           title=None, filename=None, binning='adaptive'):
+    # predict_pe = np.abs(predict_pe, 0)
+    fig = plt.figure(figsize=(8, 6))
+    pred_bin_min = np.min(predict_pe) - .001
+    pred_bin_max = np.max(predict_pe) + .001
+    if binning == 'adaptive':
+        pe = pe_truth.flatten()
+        pred = predict_pe.flatten()
+        # there must be the same number of true pes in each bin.
+        n_pe = np.sum(pe)
+        n_pe_bins = np.linspace(0, n_pe, n_bin)
+        order_prediction = np.argsort(pred)
+        cumsum_pe_ordered_pred = np.cumsum(pe[order_prediction])
+        pred_bins = [pred_bin_min, ]
+        for bin in range(n_bin - 1):
+            index_limit = np.searchsorted(
+                cumsum_pe_ordered_pred, n_pe_bins[bin]
+            )
+            pred_bins.append(pred[order_prediction[index_limit]])
+        pred_bins.append(pred_bin_max)
+        pred_bins = np.array(pred_bins)
+    elif binning == 'constant_size':
+        pred_bins = np.linspace(pred_bin_min, pred_bin_max, n_bin)
+    else:
+        raise ValueError(
+            'binning must be either \'adaptive\' or \'constant_size\'.'
+        )
+    histo_pred, _ = np.histogram(
+        predict_pe, pred_bins
     )
-    histo_photon = np.zeros_like(histo_pred)
+    histo_pe = np.zeros_like(histo_pred)
     for bin in range(len(pred_bins) - 1):
         mask_pred_bin = np.logical_and(
-            pred_bins[bin] <= predict_photon,
-            predict_photon < pred_bins[bin+1]
+            pred_bins[bin] <= predict_pe,
+            predict_pe < pred_bins[bin+1]
         )
-        histo_photon[bin] = np.sum(photon_truth[mask_pred_bin])
+        histo_pe[bin] = np.sum(pe_truth[mask_pred_bin])
     pred_bins_center = 0.5 * (pred_bins[1:] + pred_bins[:-1])
     d_pred_bins = pred_bins[1:] - pred_bins[:-1]
     plt.errorbar(
         pred_bins_center,
-        histo_photon/histo_pred,
-        xerr=d_pred_bins,
-        yerr=np.sqrt(histo_photon)/histo_pred,
+        histo_pe/histo_pred,
+        xerr=d_pred_bins/2,
+        yerr=np.sqrt(histo_pe)/histo_pred,
         fmt='.'
     )
-    plt.plot([0, np.max(pred_bins)], [0, np.max(pred_bins)], 'k--')
+    plt.plot([0, pred_bin_max], [0, pred_bin_max], 'k--')
     plt.xlabel('prediction value')
     plt.ylabel('realisation probability')
-    plt.ylim(np.array(plt.xlim())*2)
+    plt.ylim([-1e-2, plt.ylim()[1]])
     if title is not None:
         plt.title(title)
     if filename is not None:
         plt.savefig(filename)
     else:
         plt.show()
+    plt.close(fig)
 
 
-def histo_resolution(bin_size_ns, photon_truth, predict_photon,
+def histo_resolution(bin_size_ns, pe_truth, predict_pe,
                      title=None, filename=None):
-    batch_size = photon_truth.shape[0]
+    batch_size = pe_truth.shape[0]
     resolutions = []
     for i in range(batch_size):
         try:
-            _, _, width, _, _, d_width = photon_stat(
-                bin_size_ns, photon_truth[i, :], predict_photon[i, :]
+            _, _, width, _, _, d_width = pe_stat(
+                bin_size_ns, pe_truth[i, :], predict_pe[i, :]
             )
         except RuntimeError:
             print('WARNING: Gaussian fit failed')
             continue
         resolutions.extend(width[d_width < 2])
-    plt.figure(figsize=(8, 6))
+    fig = plt.figure(figsize=(8, 6))
     plt.hist(resolutions, 100)
     if title is not None:
         plt.title(title)
@@ -222,60 +255,43 @@ def histo_resolution(bin_size_ns, photon_truth, predict_photon,
         plt.savefig(filename)
     else:
         plt.show()
-
-# plt.figure()
-# plt.hist(loss, 100)
-# plt.xlabel('loss')
-# plt.ylabel('# of simulation')
-# #plt.legend()
-# plt.show()
-
-# plt.figure()
-# plt.plot(
-#     np.sum(test_photon, axis=-1),
-#     np.sum(test_waveform, axis=-1),
-#     '+', label="true"
-# )
-# plt.plot(
-#     np.sum(predict_photon, axis=-1),
-#     np.sum(test_waveform, axis=-1),
-#     '+', label='predicted'
-# )
-# plt.xlabel('# photons')
-# plt.ylabel('sum waveform')
-# plt.legend()
-# plt.show()
+    plt.close(fig)
 
 
-def init(run_name):
-    photon_rate_mhz = 20
+def demo(run_name):
+    pe_rate_mhz = 20
     batch_size = 400
-    sigma_smooth_photon_ns = 0.
+    sigma_smooth_pe_ns = 0.
 
     sampling_rate_mhz = 250
     noise = [0, 1, 2, 3, 4, 5]  # 1.05
     bin_size_ns = 0.5
     n_sample = 90
     for noise_lsb in noise:
-        waveform, photon_truth, predict_photon = run_prediction(
-            run_name, photon_rate_mhz=photon_rate_mhz,
+        title = 'p.e. rate ' + str(pe_rate_mhz) + 'MHz, noise ' + \
+                str(noise_lsb) + 'LSB'
+        waveform, pe_truth, predict_pe = run_prediction(
+            run_name, pe_rate_mhz=pe_rate_mhz,
             sampling_rate_mhz=sampling_rate_mhz, batch_size=batch_size,
             noise_lsb=noise_lsb, bin_size_ns=bin_size_ns,
-            n_sample=n_sample, sigma_smooth_photon_ns=sigma_smooth_photon_ns
+            n_sample=n_sample, sigma_smooth_pe_ns=sigma_smooth_pe_ns
         )
         t_samples_ns = np.arange(0, n_sample) * 1000 / sampling_rate_mhz
         plot_prediction(
-            bin_size_ns, photon_truth, predict_photon, t_samples_ns, waveform,
-            filename='plots/predict_noise' + str(noise_lsb) + '.png'
+            bin_size_ns, pe_truth, predict_pe, t_samples_ns, waveform,
+            filename='plots/predict_noise' + str(noise_lsb) + '.png',
+            title=title
         )
         plot_probability_check(
-            predict_photon, photon_truth,
-            filename='plots/probability_noise' + str(noise_lsb) + '.png'
+            predict_pe, pe_truth,
+            filename='plots/probability_noise' + str(noise_lsb) + '.png',
+            title=title
         )
         histo_resolution(
-            bin_size_ns, photon_truth, predict_photon,
-            filename='plots/resolution_noise' + str(noise_lsb) + '.png'
+            bin_size_ns, pe_truth, predict_pe,
+            filename='plots/resolution_noise' + str(noise_lsb) + '.png',
+            title=title
         )
 
 if __name__ == '__main__':
-    init('conv_filter488_kernel10_lr0.001_LC_rate0-200_smooth2_run0rrrr')
+    demo('cnn-example')
